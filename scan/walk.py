d1551a49792d44d4f6ca7e5b2a508eda08afd522
rx_z <orz_amber@163.com>
2017-02-07 21:46:16 +0800

    update from arch

--- scan/walk.py
+++ scan/walk.py
@@ -4,10 +4,12 @@ import os
 def iterateFIles(dir):
     assert os.path.isdir(dir), "make sure this is a path"
     result = []
+    i = 0
     for root, dirs, files in os.walk(dir, topdown=True):
         for fl in files:
             result.append(os.path.join(root, fl))
+            i = i + 1
 
-    return result
+    return i
 
 print iterateFIles('../../')
